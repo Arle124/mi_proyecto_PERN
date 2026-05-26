61423b111f5686f7302665a1eb37bea41ab5bd23
Shecid <asheryesid@email.com>
2026-05-25 21:10:31 -0500

    fix: correction restricts

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -20,7 +20,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 8 * 60 * 60 * 1000 // 8 Horas de vida en el cliente
     });
 
